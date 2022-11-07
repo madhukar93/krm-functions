863ad6e81ad8d610113f7579cde4ffa966b38117
Akshaya <39328208+Akshaya-T@users.noreply.github.com>
2022-11-07 15:33:05 +0530

    Workloads:  changed hardcoded kind in scaling (#16)
    * changed hardcoded kind in scaling
    
    * scaled object apiversion fix and added scaled object creation to rollouts as well
    
    * examples files changes
    
    * scaled object apiversion fix and added scaled object creation to rollouts as well
    
    * examples files changes
    
    * modified makeScaledObject function to receive generic object and parse as unstructured data
    
    * changed arg name in scaling
    
    Co-authored-by: Akshaya T <akshaya@Akshayas-MacBook-Pro.local>

--- pkg/workloads/scaling.go
+++ pkg/workloads/scaling.go
@@ -1,13 +1,17 @@
 package workloads
 
 import (
+	"fmt"
+
 	kedav1alpha1 "github.com/kedacore/keda/v2/apis/keda/v1alpha1"
-	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 const argoApiVersion = "argoproj.io/v1alpha1"
 const kedaTriggerAuth = "keda-trigger-auth-gcp-credentials"
+const kedaApiVersion = "keda.sh/v1alpha1"
 
 type cpu struct {
 	Target string `json:"target,omitempty"`
@@ -72,17 +76,25 @@ func (spec scalingSpec) addPubSubTrigger(so *kedav1alpha1.ScaledObject) {
 	so.Spec.Triggers = append(so.Spec.Triggers, pubsubTrigger...)
 }
 
-func (spec scalingSpec) makeScaledObject(d appsv1.Deployment) kedav1alpha1.ScaledObject {
+func (spec scalingSpec) makeScaledObject(workload metav1.Object) kedav1alpha1.ScaledObject {
+
+	u := &unstructured.Unstructured{}
+	if uc, err := runtime.DefaultUnstructuredConverter.ToUnstructured(workload); err != nil {
+		fmt.Printf("failed to convert to unstructured: %v", err)
+	} else {
+		u.SetUnstructuredContent(uc)
+	}
+
 	scaledObject := kedav1alpha1.ScaledObject{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "ScaledObject",
-			APIVersion: "keda.sh/v1alpha1",
+			APIVersion: kedaApiVersion,
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name: d.ObjectMeta.Name,
+			Name: u.GetName(),
 			Labels: map[string]string{
-				"part-of": d.ObjectMeta.Labels["part-of"],
-				"app":     d.ObjectMeta.Labels["app"],
+				"part-of": u.GetLabels()["part-of"],
+				"app":     u.GetLabels()["app"],
 			},
 		},
 		Spec: kedav1alpha1.ScaledObjectSpec{
@@ -90,9 +102,9 @@ func (spec scalingSpec) makeScaledObject(d appsv1.Deployment) kedav1alpha1.Scale
 			MaxReplicaCount: &spec.MaxReplica,
 			Triggers:        []kedav1alpha1.ScaleTriggers{},
 			ScaleTargetRef: &kedav1alpha1.ScaleTarget{
-				APIVersion: argoApiVersion,
-				Kind:       "Rollout",
-				Name:       d.ObjectMeta.Name,
+				APIVersion: u.GetAPIVersion(),
+				Kind:       u.GetKind(),
+				Name:       u.GetName(),
 			},
 		},
 	}
