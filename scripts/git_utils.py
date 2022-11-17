ce8e340f978574522db735a43addcfe63f560c34
Akshaya T <akshaya@Akshayas-MacBook-Pro.local>
2022-11-17 15:54:50 +0530

    addressed the PR comments

--- scripts/git_utils.py
+++ scripts/git_utils.py
@@ -1,7 +1,10 @@
 import requests, json
 from git import Repo
+from github import Github
+
 
 GIT_URL = "https://github.com/bukukasio/tokko-k8s"
+GIT_REPO="bukukasio/tokko-k8s"
 OWNER="bukukasio"
 REPO="tokko-k8s"
 GIT_USER=""
@@ -14,25 +17,17 @@ def git_clone_checkout(branch_name):
     return repo
 
 def git_push(repo, branch_name, function_name, new_tag):
-    try:
-        repo.git.add(update=True)
-        repo.git.commit('-m', f'krm function version upgrade: Updated the version for {function_name} function with version {new_tag}', author=f'{AUTHOR}')
-        repo.git.push('origin', branch_name)
-    except Exception as ex:
-        print(ex)
+    repo.git.add(update=True)
+    repo.git.commit('-m', f'krm function version upgrade: Updated the version for {function_name} function with version {new_tag}', author=f'{AUTHOR}')
+    repo.git.push('origin', branch_name)
 
 def create_pull_request(git_token, branch_name, function_name, new_tag):
-    headers = {
-        'Accept': 'application/vnd.github+json',
-        'Authorization': f'Bearer {git_token}',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        }
-    data = {
-            "title":"krm function version upgrade",
-            "body": f"krm function version upgrade: Updated the version for {function_name} with version {new_tag}",
-            "head": f"{GIT_USER}:{branch_name}",
-            "base": f"{BASE_BRANCH}"
-        }
-    response = requests.post(f'https://api.github.com/repos/{OWNER}/{REPO}/pulls', headers=headers, data=json.dumps(data))
-    print(response.text)
-    return response
+    g = Github(git_token)
+    repo = g.get_repo(GIT_REPO)
+    pr = repo.create_pull(
+                            title=f"krm function version upgrade for {function_name}",
+                            body=f"krm function version upgrade: Updated the version for {function_name} with version {new_tag}",
+                            head=f"{GIT_USER}:{branch_name}",
+                            base=f"{BASE_BRANCH}"
+                        )
+    print(pr)
--- scripts/krm_release.py
+++ scripts/krm_release.py
@@ -5,19 +5,19 @@ from file_utils import modify_files
 from git_utils import git_clone_checkout, git_push, create_pull_request
 
 arg_parser = argparse.ArgumentParser(description='Get the function name as input and update new release in the files accordingly')
-arg_parser.add_argument('Function',
+arg_parser.add_argument('function',
                        metavar='function',
                        type=str,
                        help='function name to update the new version tag')
-arg_parser.add_argument('New_Tag',
+arg_parser.add_argument('new_tag',
                        metavar='new_version',
                        type=str,
                        help='new version tag to be updated in the files')
 
 args = arg_parser.parse_args()
 
-function_name = args.Function
-new_tag = args.New_Tag
+function_name = args.function
+new_tag = args.new_tag
 git_token= ""
 
 branch_name=f"KRM-Func-upgrade-{function_name}-{new_tag}"
